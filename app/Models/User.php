8c1460087da5245830ff5b4d965ad8266ecdc13e
hubqin <wuhanqinb@gmail.com>
2018-10-03 16:39:00 +0000

    Add User Statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,17 @@ class User extends Authenticatable
         $this->notify(new ResetPassword($token));
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function feed()
+    {
+        $statuses = $this->statuses()
+                         ->orderBy('created_at', 'desc');
+
+        return $statuses;
+    }
+
 }
